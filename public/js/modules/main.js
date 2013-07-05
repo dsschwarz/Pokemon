010c1a6823b5bd546d6f33affe6c7f89a656a897
fm2munsh <farhanm995@gmail.com>
2013-07-05 11:39:36 -0400

    Added skeleton gamejs app

--- public/js/modules/main.js
+++ public/js/modules/main.js
@@ -1,14 +1,17 @@
-define(['gamejs', 'underscore'], function(gamejs, _) {
+define(['underscore', 'gamejs', 'modules/globals' ], function(_, gamejs, globals) {
     return function() {
+        var display = gamejs.display.setMode(globals.game.screenSize);
 
         var tick = function(msDuration) {
 
             _.each(gamejs.event.get(), function(event) {
-               console.warn(event);
+                //Handle Event
+                console.warn(event);
             });
+
             console.warn(msDuration);
         };
 
-        gamejs.time.fpsCallback(tick, this, 32);
+        gamejs.time.fpsCallback(tick, this, globals.game.fps);
     };
 });
